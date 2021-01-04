991dbc10badc9c12e9476c7f8ee3e452a5f8e52a
Murao <yoshinobu612@icloud.com>
2021-01-05 08:43:15 +0900

    first commit

---
+++ Billiards/BilliardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BilliardsApp.swift
+//  Billiards
+//
+//  Created by 村尾慶伸 on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct BilliardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
